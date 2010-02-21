c811b0135f09d778441b69c23c433259b714f288
cgreenhalgh <chris.greenhalgh@nottingham.ac.uk>
2010-02-21 20:37:25 +0000

    first commit

---
+++ src/cmg/helloandroid/Main.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package cmg.helloandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author cmg
+ *
+ */
+public class Main extends Activity {
+
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+
+}
